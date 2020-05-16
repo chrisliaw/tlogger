11117299074c88e778347e2e44a91015fc3df97c
Chris <chrisliaw@antrapol.com>
2020-05-16 10:30:11 +0800

    Shorthen the initializer to Tlogger.new

--- lib/tlogger.rb
+++ lib/tlogger.rb
@@ -8,4 +8,13 @@ require_relative "tlogger/logger_group"
 #
 module Tlogger
   class Error < StandardError; end
+
+  # shorten the initializer to Tlogger.new instead of the longer Tlogger::Tlogger.new
+  class << self
+    def new(*args,&block)
+      ::Tlogger::Tlogger.new(*args,&block) 
+    end
+  end # class self
+
 end
+
